ced977f733a6c0804bfe90b9083e00deb4ac18f5
jamie <jamie@wattu.com>
2013-10-13 23:05:34 +0100

    Added GPIO C code

--- src/main/native/GPIOController.c
+++ src/main/native/GPIOController.c
@@ -6,82 +6,85 @@
 
 JNIEXPORT jint JNICALL Java_com_wattu_sidpi_GPIOController_wiringPiSetup (JNIEnv *env, jobject thisObj) {
 	if (wiringPiSetupGpio () < 0) {
-		//fprintf (stderr, "Unable to setup wiringPi: %s\n", strerror (errno));
 		return -1;
 	}
 
    return 0;
 }
 
-JNIEXPORT void JNICALL Java_com_wattu_sidpi_GPIOController_pinMode  (JNIEnv *env, jobject thisObj, jint pin, jint mode) {
-	pinMode(pin,mode);
-}
-
 /*
  * Class:     com_wattu_sidpi_GPIOController
- * Method:    digitalWrite
+ * Method:    clockSpeed
  * Signature: (II)V
  */
-JNIEXPORT void JNICALL Java_com_wattu_sidpi_GPIOController_digitalWrite (JNIEnv *env, jobject thisObj, jint pin, jint value) {
-	digitalWrite(pin,value);
+JNIEXPORT void JNICALL Java_com_wattu_sidpi_GPIOController_clockSpeed
+  (JNIEnv * env, jobject obj, jint pin, jint speed) {
+	pinMode(pin,com_wattu_sidpi_GPIOController_MODE_CLOCK);
+	gpioClockSet(pin,speed);
 }
 
 /*
  * Class:     com_wattu_sidpi_GPIOController
- * Method:    digitalRead
+ * Method:    setPins
+ * Signature: ([I[I)V
+ */
+JNIEXPORT void JNICALL Java_com_wattu_sidpi_GPIOController_setPins
+  (JNIEnv *env, jobject obj, jintArray pins, jintArray vals) {
+	for(int i =0;i< sizeOf(pins);i++) {
+		pinMode(pin,com_wattu_sidpi_GPIOController_MODE_OUT);
+		digitalWrite(pins[i],vals[i]);
+	}
+}
+
+/*
+ * Class:     com_wattu_sidpi_GPIOController
+ * Method:    setPin
+ * Signature: (II)V
+ */
+JNIEXPORT void JNICALL Java_com_wattu_sidpi_GPIOController_setPin
+  (JNIEnv *env , jobject obj, jint pin, jint value) {
+	pinMode(pin,com_wattu_sidpi_GPIOController_MODE_OUT);
+	digitalWrite(pin,value);
+
+}
+
+/*
+ * Class:     com_wattu_sidpi_GPIOController
+ * Method:    getPin
  * Signature: (I)I
  */
-JNIEXPORT jint JNICALL Java_com_wattu_sidpi_GPIOController_digitalRead (JNIEnv *env, jobject thisObj, jint pin) {
+JNIEXPORT jint JNICALL Java_com_wattu_sidpi_GPIOController_getPin
+  (JNIEnv *env, jobject obj, jint pin) {
+	pinMode(pin,com_wattu_sidpi_GPIOController_MODE_IN);
 	return (jint) digitalRead(pin);
 }
 
 /*
  * Class:     com_wattu_sidpi_GPIOController
- * Method:    gpioClockSet
- * Signature: (II)V
+ * Method:    getPins
+ * Signature: ([I)[I
  */
-JNIEXPORT void JNICALL Java_com_wattu_sidpi_GPIOController_gpioClockSet
-  (JNIEnv *env, jobject thisObj, jint pin, jint freq) {
-	gpioClockSet(pin,freq);
-}
-
-JNIEXPORT jint JNICALL Java_com_wattu_sidpi_GPIOController_readByte (JNIEnv *env, jobject thisObj) {
-	int data = 0;
-
-	digitalWrite(8,1);
-	digitalWrite(25,0);
-	//Java_com_wattu_sidpi_GPIOController_sidWait();
-	data = digitalRead(2)
-		| (digitalRead(3) << 1)
-		| (digitalRead(17) << 2)
-		| (digitalRead(27) << 3)
-		| (digitalRead(22) << 4)
-		| (digitalRead(10) << 5)
-		| (digitalRead(9) << 6)
-		| (digitalRead(11) << 7);
-	digitalWrite(25,1);
-
-	return (jint) data;
+JNIEXPORT jintArray JNICALL Java_com_wattu_sidpi_GPIOController_getPins
+  (JNIEnv *env, jobject obj, jintArray pins) {
+	return (jintArray) 0L;
 }
 
 /*
  * Class:     com_wattu_sidpi_GPIOController
- * Method:    writeByte
- * Signature: ()V
+ * Method:    startClock
+ * Signature: (I)V
  */
-JNIEXPORT void JNICALL Java_com_wattu_sidpi_GPIOController_writeByte (JNIEnv *env, jobject thisObj, jint data) {
-	digitalWrite(8,0);
-	digitalWrite(2, (data & 1));
-	digitalWrite(3, (data & 2) >> 1);
-	digitalWrite(17, (data & 4) >> 2);
-	digitalWrite(27, (data & 8) >> 3);
-	digitalWrite(22, (data & 16) >> 4);
-	digitalWrite(10, (data & 32) >> 5);
-	digitalWrite(9, (data & 64) >> 6);
-	digitalWrite(11, (data & 128) >> 7);
-	digitalWrite(25,0);
-	//Java_com_wattu_sidpi_GPIOController_sidWait();
-	digitalWrite(25,1);
+JNIEXPORT void JNICALL Java_com_wattu_sidpi_GPIOController_startClock
+  (JNIEnv *env, jobject obj, jint pin) {
 
 }
 
+/*
+ * Class:     com_wattu_sidpi_GPIOController
+ * Method:    stopClock
+ * Signature: (I)V
+ */
+JNIEXPORT void JNICALL Java_com_wattu_sidpi_GPIOController_stopClock
+  (JNIEnv *env, jobject obj, jint pin) {
+
+}
