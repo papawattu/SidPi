453c7a25a886e8a95076ad09f0a1b9b022ac3821
jamie <jamie@wattu.com>
2013-10-21 19:56:07 +0100

    Timer

--- src/main/java/com/wattu/sidpi/SIDPiController.java
+++ src/main/java/com/wattu/sidpi/SIDPiController.java
@@ -136,10 +136,10 @@ public class SIDPiController {
 	
 	public void waitForCycles(int cycles) {
 		if(clockRunning) {
-			final long end = System.nanoTime() + (cycles * 1000);
+			final long end = System.nanoTime() + (cycles * 100);
 
 
-	        long timeLeft = cycles * 1000;
+	        long timeLeft = cycles * 100;
 	        do {
 	                try {
 						Thread.sleep (0);
