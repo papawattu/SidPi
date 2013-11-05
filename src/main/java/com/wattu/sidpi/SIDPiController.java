aecd5beb933e133eddfed35a042d98a652471f9c
jamie <jamie@wattu.com>
2013-11-05 10:45:19 +0000

    changed sid pin layout

--- src/main/java/com/wattu/sidpi/SIDPiController.java
+++ src/main/java/com/wattu/sidpi/SIDPiController.java
@@ -6,16 +6,13 @@ import com.wattu.test.sidpi.impl.GPIOControllerTestImpl;
 
 public class SIDPiController {
 
-	private static final int CS 	= GPIOController.PIN_NUMBERS[14];
-	private static final int RW 	= GPIOController.PIN_NUMBERS[15];
-	private static final int RES 	= GPIOController.PIN_NUMBERS[16];
-	private static final int CLK 	= GPIOController.PIN_NUMBERS[2];
-	private static final int[] DATA	= {GPIOController.PIN_NUMBERS[0],GPIOController.PIN_NUMBERS[1],GPIOController.PIN_NUMBERS[3]
-										,GPIOController.PIN_NUMBERS[4],GPIOController.PIN_NUMBERS[5],GPIOController.PIN_NUMBERS[6]
-										,GPIOController.PIN_NUMBERS[7],GPIOController.PIN_NUMBERS[8]};
+	private static final int CS 	= 25;
+	private static final int RW 	= 8;
+	private static final int RES 	= 7;
+	private static final int CLK 	= 4;
+	private static final int[] DATA	= {2,3,17,27,22,10,9,11};
 	
-	private static final int[] ADDR	= {GPIOController.PIN_NUMBERS[13],GPIOController.PIN_NUMBERS[12],GPIOController.PIN_NUMBERS[11]
-										,GPIOController.PIN_NUMBERS[10],GPIOController.PIN_NUMBERS[9]};
+	private static final int[] ADDR	= {24,23,18,15,14};
 			
 	private static final int DEFAULT_SID_SPEED_HZ = 1000000;
 	
