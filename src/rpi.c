3e784ec2581e665ac172f42063a388a0382eae05
jamie <jamie@wattu.com>
2014-01-12 19:42:48 +0000

    Added low-le

---
+++ src/rpi.c
@@ -0,0 +1,52 @@
+/*
+ * rpi.c
+ *
+ *  Created on: 12 Jan 2014
+ *      Author: jamie
+ */
+
+#include <stdio.h>
+
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include <unistd.h>
+#include "rpi.h"
+
+struct bcm2835_peripheral gpio = {GPIO_BASE};
+
+// Exposes the physical address defined in the passed structure using mmap on /dev/mem
+int map_peripheral(struct bcm2835_peripheral *p)
+{
+   // Open /dev/mem
+   if ((p->mem_fd = open("/dev/mem", O_RDWR|O_SYNC) ) < 0) {
+      printf("Failed to open /dev/mem, try checking permissions.\n");
+      return -1;
+   }
+
+   p->map = mmap(
+      NULL,
+      BLOCK_SIZE,
+      PROT_READ|PROT_WRITE,
+      MAP_SHARED,
+      p->mem_fd,      // File descriptor to physical memory virtual file '/dev/mem'
+      p->addr_p       // Address in physical map that we want this memory block to expose
+   );
+
+   if (p->map == MAP_FAILED) {
+        perror("mmap");
+        return -1;
+   }
+
+   p->addr = (volatile unsigned int *)p->map;
+
+   return 0;
+}
+
+void unmap_peripheral(struct bcm2835_peripheral *p) {
+
+    munmap(p->map, BLOCK_SIZE);
+    close(p->mem_fd);
+}
+
---
+++ src/rpi.h
@@ -0,0 +1,38 @@
+/*
+ * rpi.h
+ *
+ *  Created on: 12 Jan 2014
+ *      Author: jamie
+ */
+
+#ifndef RPI_H_
+#define RPI_H_
+
+
+#define BCM2708_PERI_BASE       0x20000000
+#define GPIO_BASE               (BCM2708_PERI_BASE + 0x200000)	// GPIO controller
+
+#define BLOCK_SIZE 		(4*1024)
+
+// IO Access
+struct bcm2835_peripheral {
+    unsigned long addr_p;
+    int mem_fd;
+    void *map;
+    volatile unsigned int *addr;
+};
+
+struct bcm2835_peripheral gpio = {GPIO_BASE};
+
+extern struct bcm2835_peripheral gpio;  // They have to be found somewhere, but can't be in the header
+
+#define INP_GPIO(g)   *(gpio.addr + ((g)/10)) &= ~(7<<(((g)%10)*3))
+#define OUT_GPIO(g)   *(gpio.addr + ((g)/10)) |=  (1<<(((g)%10)*3))
+#define SET_GPIO_ALT(g,a) *(gpio.addr + (((g)/10))) |= (((a)<=3?(a) + 4:(a)==4?3:2)<<(((g)%10)*3))
+
+#define GPIO_SET  *(gpio.addr + 7)  // sets   bits which are 1 ignores bits which are 0
+#define GPIO_CLR  *(gpio.addr + 10) // clears bits which are 1 ignores bits which are 0
+
+#define GPIO_READ(g)  *(gpio.addr + 13) &= (1<<(g))
+
+#endif /* RPI_H_ */
