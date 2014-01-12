524bd7d5b84c83e6e91617ff4c70904068156b0c
jamie <jamie@wattu.com>
2014-01-12 19:39:20 +0000

    Moved to src folder

--- src/SidRunnerThread.h
+++ src/SidRunnerThread.h
@@ -8,7 +8,16 @@
 #ifndef SIDRUNNERTHREAD_H_
 #define SIDRUNNERTHREAD_H_
 
+#define CS 	7
+#define RW 	0
+#define RES 0
+#define CLK 4
+
+const int DATA[]	= {2,3,17,27,22,10,9,11};
+const int ADDR[]	= {8,25,24,23,18};
+
 void sidDelay(int cycles);
 void sidWrite(int reg,int value,int writeCycles);
+
 
 #endif /* SIDRUNNERTHREAD_H_ */
