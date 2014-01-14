370b17b59742b876293d04d0ec861d120bd77e90
jamie <jamie@wattu.com>
2014-01-14 19:37:17 +0000

    .

---
+++ src/testSid.c
@@ -0,0 +1,20 @@
+/*
+ * testSid.c
+ *
+ *  Created on: 14 Jan 2014
+ *      Author: jamie
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include "SidRunnerThreadh.h"
+
+int main(void) {
+
+
+
+	return 0;
+}
+
