c9d44d264c103d20664e0e56aa08e0b4339114b8
jamie <jamie@wattu.com>
2014-01-14 16:30:05 +0000

    .

--- src/SidRunnerThread.c
+++ src/SidRunnerThread.c
@@ -97,7 +97,7 @@ void delay(int cycles) {
 
 }
 void writeSid(int reg, int val) {
-	print("reg : %d val : %d \n",reg,val);
+	printf("reg : %d val : %d \n",reg,val);
 	*(gpio.addr + 10) = (unsigned long) 1 << CS;
 	*(gpio.addr + 7) = (unsigned long) dataPins[val % 256] | addrPins[reg % 32];
 	*(gpio.addr + 10) = (unsigned long) (~dataPins[val % 256] & dataPins[255])
