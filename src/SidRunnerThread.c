5aa76ce277b0342594a4a2b1be6ab7a5fd82b413
jamie <jamie@wattu.com>
2014-01-19 10:04:40 +0000

    .

--- src/SidRunnerThread.c
+++ src/SidRunnerThread.c
@@ -13,9 +13,9 @@ pthread_t sidThreadHandle;
 
 typedef struct buffer {
         unsigned char q[BUFFER_SIZE+4];		/* body of queue */
-        int first;                      /* position of first element */
-        int last;                       /* position of last element */
-        int count;                      /* number of queue elements */
+        long first;                      /* position of first element */
+        long last;                       /* position of last element */
+        long count;                      /* number of queue elements */
 } Buffer;
 
 Buffer buffer;
