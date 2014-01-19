ef1e01f4e9d576acdb47cd4e3c5b0a0deaf084b4
jamie <jamie@wattu.com>
2014-01-19 09:39:59 +0000

    .

--- src/SidPiServer.c
+++ src/SidPiServer.c
@@ -239,10 +239,8 @@ void processReadBuffer(int len) {
 			break;
 		}
 
-	//	if(handleWritePacket(dataLength) >= 0)
-			dataWrite[dataWritePos++] = OK;
-	//	else
-//			dataWrite[dataWritePos++] = BUSY;
+		handleWritePacket(dataLength);
+
 		break;
 	}
 
@@ -343,7 +341,7 @@ int handleWritePacket(int dataLength) {
 	unsigned int i,writeCycles;
 	unsigned char reg,sid,value;
 
-	if((dataLength *4) + getBufferCount() >= getBufferMax()) return -1;
+	//if((dataLength *4) + getBufferCount() >= getBufferMax()) return -1;
 
 	for (i = 0; i < dataLength; i += 4) {
 		writeCycles = (int) ((dataRead[4 + i] & 0xff) << 8) | dataRead[5 + i];
