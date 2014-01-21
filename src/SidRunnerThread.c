c63a8c0cbf00f8497a1561fea67cb648be2bcc34
jamie <jamie@wattu.com>
2014-01-21 13:46:35 +0000

    .

--- src/SidRunnerThread.c
+++ src/SidRunnerThread.c
@@ -119,11 +119,18 @@ void sidWrite(int reg, int value, int cycleHigh,int cycleLow) {
 }
 void delay(int cycles) {
 	struct timespec tim;
+	long targetCycles = getRealSidClock() + cycles;
+
+	while(getRealSidClock() < targetCycles);
+
+	return;
 	tim.tv_sec = 0;
 	tim.tv_nsec = (long) cycles * 900;
 	if(cycles <= 8) return;
 
 	nanosleep(&tim,NULL);
+
+
 }
 
 long getSidClock() {
