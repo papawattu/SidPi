e24d15b9802b8b73aa9a16cdc2381428404d8f85
jamie <jamie@wattu.com>
2014-01-21 22:37:53 +0000

    .

--- src/SidRunnerThread.c
+++ src/SidRunnerThread.c
@@ -113,17 +113,18 @@ void sidWrite(int reg, int value, int cycleHigh,int cycleLow) {
 
 }
 void delay(int cycles) {
-	/*struct timespec tim;
+	struct timespec tim;
 	int threshold = 10,multi = 1000;
 	long current;
-*/	long targetCycles = (getRealSidClock() + cycles) ;
-	//if(cycles <=1) return;
-/*	tim.tv_sec = 0;
-	tim.tv_nsec = (long) (cycles * multi) - 100;
-	nanosleep(&tim,NULL);
-	//printf("Difference : %d\n" ,getRealSidClock() - targetCycles);
-*/
-	while(getRealSidClock() < targetCycles);
+	long targetCycles = (getRealSidClock() + cycles) ;
+	while(getRealSidClock() < targetCycles) {
+		if(cycles > 80) {
+			tim.tv_sec = 0;
+			tim.tv_nsec = (long) (cycles * 1000) - 80;
+			nanosleep(&tim,NULL);
+			return;
+		}
+	}
 
 }
 long getSidClock() {
