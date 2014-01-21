cdebef47caeedac431d3aa19334dbfda77f091e2
jamie <jamie@wattu.com>
2014-01-21 20:47:18 +0000

    .

--- src/SidRunnerThread.c
+++ src/SidRunnerThread.c
@@ -118,10 +118,11 @@ void delay(int cycles) {
 	long targetCycles = getRealSidClock() + cycles;
 
 	while((current = getRealSidClock()) < targetCycles) {
-		if((cycles - 200) > 1000) {
+		if(cycles > 1200) {
 			tim.tv_sec = 0;
-			tim.tv_nsec = (long) cycles * 100;
+			tim.tv_nsec = (long) cycles * 56;
 			nanosleep(&tim,NULL);
+			cycles -=1200;
 		}
 	}
 	//printf("target cycles : %08x\t now : %08x\tdifference : %08x\n",targetCycles, current,current - targetCycles);
