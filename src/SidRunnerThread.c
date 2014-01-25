a7facda0b47211122777854a0b7dd88e25ceba62
jamie <jamie@wattu.com>
2014-01-25 19:14:25 +0000

    .

--- src/SidRunnerThread.c
+++ src/SidRunnerThread.c
@@ -52,7 +52,7 @@ void setupSid() {
 void startSidThread() {
 	struct sched_param params;
 	int ret;
-	params.sched_priority = sched_get_priority_max(SCHED_OTHER) ;
+	params.sched_priority = 40 ;
 
 	if (pthread_create(&sidThreadHandle, NULL, sidThread, NULL) == -1)
 			perror("cannot create Sid thread");
