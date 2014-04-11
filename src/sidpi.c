c7722e6f6f2bd504b9b10c7c60be4b9398a22c65
jamie <jamie@wattu.com>
2014-04-11 23:15:11 +0100

    .

--- src/sidpi.c
+++ src/sidpi.c
@@ -26,7 +26,7 @@ static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
  * Global variables are declared as static, so are global within the file. 
  */
 
-static int Device_Open = 0;
+
 static int Major;		/* Major number assigned to our device driver */
 static int Device_Open = 0;	/* Is device open?  
 				 * Used to prevent multiple access to device */
