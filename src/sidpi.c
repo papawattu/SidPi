92b30d709b2dca84afe3bdeb4fd2b2891e87846f
jamie <jamie@wattu.com>
2014-04-11 23:30:52 +0100

    .

--- src/sidpi.c
+++ src/sidpi.c
@@ -21,7 +21,7 @@ static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
 #define SUCCESS 0
 #define DEVICE_NAME "sid0"	/* Dev name as it appears in /proc/devices   */
 #define BUF_LEN 80		/* Max length of the message from the device */
-#define MAJOR_NUM 664
+#define MAJOR_NUM 164
 
 /* 
  * Global variables are declared as static, so are global within the file. 
