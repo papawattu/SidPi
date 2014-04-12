58e1e46da8ab8eeb85e2db004f974b4e1c3223ff
jamie <jamie@wattu.com>
2014-04-12 20:43:18 +0100

    .

--- src/sidpi.c
+++ src/sidpi.c
@@ -10,7 +10,7 @@
 #include <linux/delay.h>
 //#include <asm/uaccess.h>	/* for put_user */
 #include <linux/proc_fs.h>	/* Necessary because we use the proc fs */
-
+#include <linux/seq_file.h>
 #include "sidpithread.h"
 
 #define procfs_name "sidpi"
@@ -58,54 +58,28 @@ static int __init _sid_init_module(void)
 		return Major;
 	}
 
-	Our_Proc_File = create_proc_entry(procfs_name, 0644, NULL);
-
-	if (Our_Proc_File == NULL) {
-		remove_proc_entry(procfs_name, &proc_root);
-		printk(KERN_ALERT "Error: Could not initialize /proc/%s\n",
-				procfs_name);
-		return -ENOMEM;
-	}
-
-	Our_Proc_File->read_proc = procfile_read;
-	Our_Proc_File->owner = THIS_MODULE;
-	Our_Proc_File->mode = S_IFREG | S_IRUGO;
-	Our_Proc_File->uid = 0;
-	Our_Proc_File->gid = 0;
-	Our_Proc_File->size = 37;
+	proc_create("hello_proc", 0, NULL, &hello_proc_fops);
 	//setupSid();
 
 	return SUCCESS;
 }
-int procfile_read(char *buffer,
-	      char **buffer_location,
-	      off_t offset, int buffer_length, int *eof, void *data)
-{
-	int ret;
 
-	printk(KERN_INFO "procfile_read (/proc/%s) called\n", procfs_name);
-
-	/*
-	 * We give all of our information in one go, so if the
-	 * user asks us if we have more information the
-	 * answer should always be no.
-	 *
-	 * This is important because the standard read
-	 * function from the library would continue to issue
-	 * the read system call until the kernel replies
-	 * that it has no more information, or until its
-	 * buffer is filled.
-	 */
-	if (offset > 0) {
-		/* we have finished to read, return 0 */
-		ret  = 0;
-	} else {
-		/* fill the buffer, return the buffer size */
-		ret = sprintf(buffer, "HelloWorld!\n");
-	}
-
-	return ret;
+static int hello_proc_show(struct seq_file *m, void *v) {
+  seq_printf(m, "Hello proc!\n");
+  return 0;
 }
+
+static int hello_proc_open(struct inode *inode, struct  file *file) {
+  return single_open(file, hello_proc_show, NULL);
+}
+
+static const struct file_operations hello_proc_fops = {
+  .owner = THIS_MODULE,
+  .open = hello_proc_open,
+  .read = seq_read,
+  .llseek = seq_lseek,
+  .release = single_release,
+};
 /*
  * This function is called when the module is unloaded
  */
@@ -116,7 +90,7 @@ static void __exit _sid_cleanup_module(void)
 	 * Unregister the device 
 	 */
 	unregister_chrdev(MAJOR_NUM, DEVICE_NAME);
-	remove_proc_entry(procfs_name, &proc_root);
+	remove_proc_entry("hello_proc", NULL);
 	//closeSid();
 }
 static int device_open(struct inode *inode, struct file *file) {
