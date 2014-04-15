46b449edf2122da2483d00f984757b1187ec08a0
jamie <jamie@wattu.com>
2014-04-15 20:00:05 +0100

    .

--- src/sidpithread.c
+++ src/sidpithread.c
@@ -228,7 +228,7 @@ long getSidClock(void) {
 unsigned long long int getRealSidClock(void) {
 	//unsigned long long int * clock = (unsigned long long int *) (ioread32(gpio_timer +TIMER_OFFSET) |
 	//		(ioread32(gpio_timer + TIMER_OFFSET + 4) << 32));
-	return 0;*clock;
+	return 0;//*clock;
 }
 void writeSid(int reg, int val) {
 	iowrite32((unsigned long) addrPins[reg % 32],(u32 *) gpio + 7);
