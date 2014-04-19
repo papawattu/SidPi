fef0f67d85437607fbdfee214c1c045015938464
jamie <jamie@wattu.com>
2014-04-19 23:04:51 +0100

    .

--- src/sidpithread.c
+++ src/sidpithread.c
@@ -136,7 +136,7 @@ int sidThread(void) {
 
 		if (signal_pending(current))
 			break;
-		if (buffer.count > 3) {
+		if (buffer.count > 4096) {
 			reg = dequeue(&buffer);
 			val = dequeue(&buffer);
 
