27622a7a664cd8696f0b4a682fa1364cd3fc8822
jamie <jamie@wattu.com>
2014-04-19 21:36:11 +0100

    .

--- src/sidpithread.c
+++ src/sidpithread.c
@@ -136,7 +136,7 @@ int sidThread(void) {
 
 		if (signal_pending(current))
 			break;
-		if (buffer.count > 3) {
+		if (buffer.count > 1000) {
 			reg = dequeue(&buffer);
 			val = dequeue(&buffer);
 
@@ -269,9 +269,8 @@ void writeSid(int reg, int val) {
 	iowrite32((unsigned long) 1 << CS, (u32 *) gpio + 10);
 	iowrite32((unsigned long) dataPins[val % 256], (u32 *) gpio + 7);
 	iowrite32((unsigned long) ~dataPins[val % 256] & dataPins[255], (u32 *) gpio + 10);
-	udelay(4);
+	udelay(100);
 	iowrite32((unsigned long) 1 << CS, (u32 *) gpio + 7);
-	udelay(4);
 
 }
 void startSidClk(int freq) {
