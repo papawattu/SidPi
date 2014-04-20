b9d359e28fbd38f9bc67431ebf9e4f6da54380c4
jamie <jamie@wattu.com>
2014-04-21 00:06:43 +0100

    .

--- src/sidpithread.c
+++ src/sidpithread.c
@@ -225,7 +225,7 @@ void delay(unsigned int howLong) {
 
 		memcpy(&lasttv, &tv, sizeof(tv));
 
-		//cycles -= clocks;
+		cycles -= clocks;
 
 		//printk(KERN_INFO "1 Clocks %lu Delay %d Last Clock %lu Difference %lu\n",clocks,howLong,lastClock,getRealSidClock() - lastClock);
 		while (cycles > 250000 / HZ ) {
@@ -238,7 +238,7 @@ void delay(unsigned int howLong) {
 			           + ( tv.tv_usec - lasttv.tv_usec);
 
 			memcpy(&lasttv, &tv, sizeof(tv));
-			//cycles -= clocks;
+			cycles -= clocks;
 		}
 
 		if (cycles > 4 ) {
