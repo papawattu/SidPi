0bfbaf90ad6c59a934aa219e375e6998582d356e
jamie <jamie@wattu.com>
2014-04-20 21:54:37 +0100

    .

--- src/sidpithread.c
+++ src/sidpithread.c
@@ -271,9 +271,8 @@ void writeSid(int reg, int val) {
 	iowrite32((unsigned long) 1 << CS, (u32 *) gpio + 10);
 	iowrite32((unsigned long) dataPins[val % 256], (u32 *) gpio + 7);
 	iowrite32((unsigned long) ~dataPins[val % 256] & dataPins[255], (u32 *) gpio + 10);
-	udelay(1000);
+	udelay(100);
 	iowrite32((unsigned long) 1 << CS, (u32 *) gpio + 7);
-	udelay(1000);
 
 	//udelay(15);
 
