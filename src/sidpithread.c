ced009f54f65931067e9291bbcf6da2ed54dba44
jamie <jamie@wattu.com>
2014-04-20 23:51:57 +0100

    .

--- src/sidpithread.c
+++ src/sidpithread.c
@@ -228,10 +228,10 @@ void delay(unsigned int howLong) {
 		cycles -= clocks;
 
 		//printk(KERN_INFO "1 Clocks %lu Delay %d Last Clock %lu Difference %lu\n",clocks,howLong,lastClock,getRealSidClock() - lastClock);
-	/*	while (cycles > 10000 / HZ ) {
+		while (cycles > 250000 / HZ ) {
 
 			current->state = TASK_INTERRUPTIBLE;
-			schedule_timeout(cycles / 10000);
+			schedule_timeout(cycles / 250000);
 			do_gettimeofday(&tv);
 
 			clocks = (tv.tv_sec - lasttv.tv_sec) * 1000000
@@ -240,7 +240,7 @@ void delay(unsigned int howLong) {
 			memcpy(&lasttv, &tv, sizeof(tv));
 			cycles -= clocks;
 		}
-*/
+
 		if (cycles > 4 ) {
 			udelay(clocks);
 		}
