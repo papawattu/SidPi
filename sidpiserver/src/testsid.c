f7488c7098a0580e394b2e3e68f009657566f755
jamie <jamie@wattu.com>
2014-08-12 08:18:34 +0100

    .

--- sidpiserver/src/testsid.c
+++ sidpiserver/src/testsid.c
@@ -8,8 +8,13 @@ int main(int argc, char * argv[]) {
 	//if(argc != 3) return -1;
 
 	printf("Setting up SID ...\n");
-	printf("Reg %d value %d\n", atoi(argv[1]), atoi(argv[2]));
-	setup_sid();
-	write_sid(atoi(argv[1]),atoi(argv[2]));
+	//printf("Reg %d value %d\n", atoi(argv[1]), atoi(argv[2]));
+	//setup_sid();
 
+	for(i=0;i<100;i++) {
+		write_bit(1);
+		sleep(1);
+		write_bit(0);
+		sleep(1);
+	}
 }
