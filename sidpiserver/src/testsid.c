62f63188594d6bbef0c60db590b8a49c7186efb3
jamie <jamie@wattu.com>
2014-08-12 20:18:04 +0100

    .

--- sidpiserver/src/testsid.c
+++ sidpiserver/src/testsid.c
@@ -16,10 +16,10 @@ int main(int argc, char * argv[]) {
 
 	for(i=0;i<100;i++) {
 		write_bit(1);
-		GPIO_SET = 1 << 3;
+		//GPIO_SET = 1 << 3;
 		sleep(1);
 		write_bit(0);
-		GPIO_CLR = 1 << 3;
+		//GPIO_CLR = 1 << 3;
 
 		sleep(1);
 	}
